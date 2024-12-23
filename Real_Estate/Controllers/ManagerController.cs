9fe3e8caaef5467f59046750e983085b259192c3
Sarfaraj0001 <sarfrajbabu786@gmail.com>
2024-12-24 02:30:14 +0530

    Add project files.

---
+++ Real_Estate/Controllers/ManagerController.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Real_Estate.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Printing()
+        {
+            return View();
+        }
+
+        public ActionResult Printing_Form()
+        {
+            return View();
+        }
+
+        //Customer Wlcome Letter
+        public ActionResult Printing_cwl()
+        {
+            return View();
+        }
+
+        //Customer 
+        public ActionResult Printing_customer()
+        {
+            return View();
+        }
+
+        // Pending Customer 
+        public ActionResult Printing_pc()
+        {
+            return View();
+        }
+
+        // Customer EMI Allotment Letter 
+        public ActionResult Printing_ceal()
+        {
+            return View();
+        }
+
+        // Customer Part Payment Allotment Letter 
+        public ActionResult Printing_cppal()
+        {
+            return View();
+        }
+
+        // Customer Full Payment Allotment Letter
+        public ActionResult Printing_cfpal()
+        {
+            return View();
+        }
+
+        // Possession Report
+        public ActionResult Printing_pr()
+        {
+            return View();
+        }
+
+        // Renewal
+        public ActionResult Printing_renewal()
+        {
+            return View();
+        }
+
+        // Pending Renewal
+        public ActionResult Printing_prenewal()
+        {
+            return View();
+        }
+
+        // Spot Commission
+        public ActionResult Printing_sc()
+        {
+            return View();
+        }
+
+        // AGREEMENT TO SELL PROPEERTY (EMI)
+        public ActionResult Printing_atsp_emi()
+        {
+            return View();
+        }
+
+        // Deal Sheet Report
+        public ActionResult Printing_dsr()
+        {
+            return View();
+        }
+        // Create ---------------------------------------------------------------------------------
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // craete collection point
+        public ActionResult Create_cp()
+        {
+            return View();
+        }
+
+        // craete associate
+        public ActionResult Create_associate()
+        {
+            return View();
+        }
+
+        // craete customer
+        public ActionResult Create_customer()
+        {
+            return View();
+        }
+
+        // craete FD customer
+        public ActionResult Create_fdcustomer()
+        {
+            return View();
+        }
+
+        // craete Member
+        public ActionResult Create_member()
+        {
+            return View();
+        }
+
+        // Addvance Booking
+        public ActionResult Create_ab()
+        {
+            return View();
+        }
+
+        // Add Expense
+        public ActionResult Create_aa()
+        {
+            return View();
+        }
+
+        // ID Card
+        public ActionResult Create_IdCard()
+        {
+            return View();
+        }
+
+        // Create Receptionist
+        public ActionResult Create_cr()
+        {
+            return View();
+        }
+        // payment ---------------------------------------------------------------------------------
+        public ActionResult Payment()
+        {
+            return View();
+        }
+
+        //Emi payment 
+        public ActionResult Payment_emipayment()
+        {
+            return View();
+        }
+
+        //Part/ CLP/ 50-50 Payment 
+        public ActionResult Payment_pcp()
+        {
+            return View();
+        }
+
+        //Associate Advance 
+        public ActionResult Payment_aa()
+        {
+            return View();
+        }
+
+        //Spot Commission 
+        public ActionResult Payment_sc()
+        {
+            return View();
+        }
+
+        // Update  ---------------------------------------------------------------------------------
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        // Collection point 
+        public ActionResult Update_cp()
+        {
+            return View();
+        }
+
+        // Collection point 
+        public ActionResult Update_c()
+        {
+            return View();
+        }
+
+        // Associate Mobile No 
+        public ActionResult Update_amn()
+        {
+            return View();
+        }
+
+        // Customer Mobile No 
+        public ActionResult Update_cmn()
+        {
+            return View();
+        }
+
+        // Customer Mobile No 
+        public ActionResult Update_Akyc()
+        {
+            return View();
+        }
+
+        // Fund Management ---------------------------------------------------------------------------------
+        public ActionResult Fund_management()
+        {
+            return View();
+        }
+
+        // Daily Closing 
+        public ActionResult Fund_dc()
+        {
+            return View();
+        }
+
+        // Cheque Registor 
+        public ActionResult Fund_cr()
+        {
+            return View();
+        }
+
+        // Cheque Registor 
+        public ActionResult Fund_btr()
+        {
+            return View();
+        }
+
+        // Maturaty ---------------------------------------------------------------------------------
+        public ActionResult Maturity()
+        {
+            return View();
+        }
+
+        // Maturity Paid Details
+        public ActionResult Maturity_mpd()
+        {
+            return View();
+        }
+
+        // Issue Discharge Form
+        public ActionResult Maturity_idf()
+        {
+            return View();
+        }
+
+        // Full
+        public ActionResult Maturity_full()
+        {
+            return View();
+        }
+
+        // Laps
+        public ActionResult Maturity_laps()
+        {
+            return View();
+        }
+
+        // Pre
+        public ActionResult Maturity_pre()
+        {
+            return View();
+        }
+
+        // Maturity Form
+        public ActionResult Maturity_mf()
+        {
+            return View();
+        }
+        // Field Reports ---------------------------------------------------------------------------------
+        public ActionResult Field_reports()
+        {
+            return View();
+        }
+        public ActionResult Field_amp()
+        {
+            return View();
+        }
+
+
+        // Official Reports ---------------------------------------------------------------------------------
+        public ActionResult Official_reports()
+        {
+            return View();
+        }
+
+        // Search ---------------------------------------------------------------------------------
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
